823afbebf8843797ba4e37e292b3b73f30b65829
Shaheer Ahmed <138614123+sherubar@users.noreply.github.com>
2023-08-29 18:03:56 +0400

    Connect HTML btns to JS and add events.
    Linked the display screen as well as all the buttons
    on the calculator to JS using querySelector in order
    to allow the user to carry out the calculations they want to.
    Clicking any of the number buttons will input said number onto
    the display screen to let the user know what they are inputting.
    AC button will clear the display fully if the user chooses to do so.
    C button will remove the character last entered by the user if
    they entered something by mistake. Operator buttons will be disabled
    if display is currently showing "ERROR!". "ERROR!" can only
    be caused by if the user attempts to divide a value with 0.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-let num1 = 0
-let num2 = 0
-let operator = ""
+const displayScreen = document.querySelector(".display-screen")
+const numberBtns = document.querySelectorAll(".number")
+const operatorBtns = document.querySelectorAll(".operator")
+const equalBtn = document.querySelector(".equal")
+const acBtn = document.querySelector(".ac-btn")
+const cBtn = document.querySelector(".c-btn")
+displayScreen.textContent = ""
 
 function add(a, b) {
-    console.log(a + b)
+    displayScreen.textContent = Math.round((a + b) * 1000) / 1000
 }
 
 function subtract(a, b) {
-    console.log(a - b)
+    displayScreen.textContent = Math.round((a - b) * 1000) / 1000
 }
 
 function multiply(a, b) {
-    console.log(a * b)
+    displayScreen.textContent = Math.round((a * b) * 1000) / 1000
 }
 
 function divide(a, b) {
-    console.log(a / b)
+    displayScreen.textContent = Math.round((a / b) * 1000) / 1000
 }
 
 function operate(a, b, operator) {
@@ -30,8 +34,77 @@ function operate(a, b, operator) {
     else if (operator === "x") {
         multiply(num1, num2)
     }
-    else if (operator === "/") {
+    else if (operator === "÷") {
+        if (num2 === 0) {
+            displayScreen.textContent = "ERROR!"
+        }
+        else {
         divide(num1, num2)
+        }
     }
 }
-    
+
+numberBtns.forEach(number => {
+    number.addEventListener('click', () => {
+        if (displayScreen.textContent === "ERROR!") {
+            displayScreen.textContent = ""
+            displayScreen.textContent += number.textContent
+        }
+        else {
+        displayScreen.textContent += number.textContent
+        }
+    })
+})
+
+operatorBtns.forEach(operator => {
+    operator.addEventListener('click', () => {
+        let operatorVal = operator.textContent
+        let scrn = displayScreen.textContent
+        if (scrn.includes("+") || scrn.includes("-") || scrn.includes("x") || scrn.includes("÷")) {
+            const [num1, operator, num2] = displayScreen.textContent.split(" ")
+            val1 = Number(num1)
+            val2 = Number(num2)
+            result = operate(val1, val2, operator)
+            if (displayScreen.textContent === "ERROR!") {
+                displayScreen.textContent = "ERROR!"
+            }
+            else {
+            displayScreen.textContent += ` ${operatorVal} `
+            }
+        }
+        else {
+            if (displayScreen.textContent === "ERROR!") {
+                displayScreen.textContent = "ERROR!"
+            }
+            else {
+            displayScreen.textContent += ` ${operatorVal} `
+            }
+        }
+    })
+})
+
+equalBtn.addEventListener('click', () => {
+    if (displayScreen.textContent === "") {
+        displayScreen.textContent = ""
+    }
+    const [num1, operator, num2] = displayScreen.textContent.split(" ") 
+    val1 = Number(num1)
+    val2 = Number(num2)
+    result = operate(val1, val2, operator)
+})
+
+cBtn.addEventListener('click', () => {
+    if (displayScreen.textContent.slice(-1) === " ") {
+        displayScreen.textContent = displayScreen.textContent.slice(0, -3)
+    }
+    else {
+        displayScreen.textContent = displayScreen.textContent.slice(0, -1)
+    }
+})
+
+acBtn.addEventListener('click', () => {
+    if (displayScreen.textContent != "") {
+        displayScreen.textContent = ""
+        val1 = ""
+    }
+})
